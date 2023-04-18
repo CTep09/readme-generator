796f4eb3a8cc576c51ff163fd7d9f1e179bd958d
Cassie T <cassandray9@gmail.com>
2023-04-18 17:20:43 -0600

    function to write Readme file created

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+}
+];
+
+inquirer.prompt(questions)
+.then((response) => {
+    console.log(response);
+    writeToFile('readMeTest.md', response)
+})
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const outputString = `# ${data.title}`
+ 
+    fs.writeFile(fileName, outputString, function(error) {
+        if (error) {
+            console.error("Error writing to file: ", error);
+        } else {
+            console.log('Success!');
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+
